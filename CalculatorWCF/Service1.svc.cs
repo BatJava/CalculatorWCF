537f151a7c2b0cfb778d6d2c00ae1912a29c1c13
@BatJava <micl@easj.dk>
2017-10-22 19:34:30 +0200

    Add project files.

---
+++ CalculatorWCF/Service1.svc.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CalculatorWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+
+
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double div(int a, int b)
+        {
+            return 1.0*a/b;
+        }
+
+        public List<String> names()
+        {
+            List<String> dictionary = new List<String>();
+            dictionary.Add("Anders");
+            dictionary.Add("Baby");
+            dictionary.Add("Claus");
+            return dictionary;
+
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
